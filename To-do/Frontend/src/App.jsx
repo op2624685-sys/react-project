430a3c1b60b939806138968a90be33b6ea3597a1
Om Prakash <op2624685@gmail.com>
2026-01-08 22:57:32 +0530

    card btn

--- To-do/Frontend/src/App.jsx
+++ To-do/Frontend/src/App.jsx
@@ -1,36 +1,24 @@
+import Card from "./components/card";
+
 const App = () => {
   return (
     <div>
       <h2>My TO-DO List</h2>
       <div className="container">
-      <div id="cards">
-        <div id="card-1" className="cards">
-          <h3>Task 1</h3>
-          <p>This is the first task.</p>
-        </div>
-        <div id="card-2" className="cards">
-          <h3>Task 2</h3>
-          <p>This is the second task.</p>
-        </div>
-        <div id="card-3" className="cards">
-          <h3>Task 3</h3>
-          <p>This is the third task.</p>
-        </div>
-        <div id="card-4" className="cards">
-          <h3>Task 4</h3>
-          <p>This is the fourth task.</p>
-        </div>
-        <div id="card-5" className="cards">
-          <h3>Task 5</h3>
-          <p>This is the fifth task.</p>
-        </div>
-        <div id="card-6" className="cards">
-          <h3>Task 6</h3>
-          <p>This is the sixth task.</p>
-        </div>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         </div>
       </div>
-    </div>
   )
 }
 
---
+++ To-do/Frontend/src/components/Card.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+        <h3>Sample To-Do Item</h3>
+        <p>This is a description of the to-do item.</p>
+        <button id="delete-btn" type="button">Delete</button>
+        <button id="edit-btn" type="button">Edit</button>
+    </div>
+  )
+}
+
+export default Card
